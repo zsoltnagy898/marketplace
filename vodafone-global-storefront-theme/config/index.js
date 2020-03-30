cf84301590463feec33c0b5995debaed5d1af000
Martin Horvath <martin.horvath@attrecto.com>
2020-03-30 16:44:32 +0200

    VOD - 23 - search basic layout

--- vodafone-global-storefront-theme/config/index.js
+++ vodafone-global-storefront-theme/config/index.js
@@ -3,10 +3,10 @@
 const config = {
 	env: "dev",
 	theme: "vodafone",
-	marketplacePath: "https://testvfgroup.appdirect.com/home",
+	marketplacePath: "https://testvfspain.appdirect.com/home",
 	// marketplacePath: "https://cloud-apps.vodafone.com/home",
 	// pageObjectsPath: "https://marketplace.appdirect.com/api/internal/storefront/v1",
-	pageObjectsPath: "https://testvfgroup.appdirect.com/api/internal/storefront/v1",
+	pageObjectsPath: "https://testvfspain.appdirect.com/api/internal/storefront/v1",
 	publicCSSGenPath: "/assets/css/pages",
 	publicJSPath: "/assets/css/images",
 	publicImagesPath: "/assets/js",

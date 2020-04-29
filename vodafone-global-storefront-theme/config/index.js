8ac8946d08306e52881ff69db790400daa6d9f51
Martin Horvath <martin.horvath@attrecto.com>
2020-04-29 10:36:25 +0200

    Example faulty logged-out header

--- vodafone-global-storefront-theme/config/index.js
+++ vodafone-global-storefront-theme/config/index.js
@@ -3,8 +3,8 @@
 const config = {
 	env: "dev",
 	theme: "vodafone",
-	marketplacePath: "https://testvfspain.appdirect.com/home",
-	// marketplacePath: "https://cloud-apps.vodafone.com/home",
+	marketplacePath: "https://testvfspain.appdirect.com",
+	// marketplacePath: "https://cloud-apps.vodafone.com",
 	// pageObjectsPath: "https://marketplace.appdirect.com/api/internal/storefront/v1",
 	pageObjectsPath: "https://testvfspain.appdirect.com/api/internal/storefront/v1",
 	publicCSSGenPath: "/assets/css/pages",
